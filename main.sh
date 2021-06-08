8b750408583fdfc568658f99ff0b80e417fc5d25
Nirbhay Gaur <nirbhaygaur.cer18@itbhu.ac.in>
2021-06-08 18:01:45 +0530

    more functions

--- main.sh
+++ main.sh
@@ -12,11 +12,15 @@
 ### CONSTANTS ###
 PROGNAME="$(basename $0)"
 DATA_FILE="./$PROGNAME.data_file"
+EXCODE="$?"
 #RIGHT_NOW="$(date +"%x %r")"
 
 ### VARIABLES ###
 task=
-id=0
+id=
+line_num=
+replace_text=
+opt=
 
 ### FUNCTIONS ###
 
@@ -48,16 +52,68 @@ id=0
 #    touch $DATA_FILE
 #}
 
+confirm()
+{
+    while true; do
+        read -p "Are you sure? [y/n]: " opt 
+        case $opt in 
+            [Yy]* )  return 0;;
+            [Nn]* )  return 1;;
+            *     )  echo "Err...answer in yes or no, dumbo!" 
+        esac
+    done
+}
+
 add()
 {
     if [ -n "$task" ]; then
-        echo "$id - $task" >> $DATA_FILE
-        
+        echo "$task" >> $DATA_FILE
     else 
         read -p "Enter task: " task
-        echo "$id - $task" >> $DATA_FILE
+        echo "$task" >> $DATA_FILE
     fi
-    ((id++))
+    echo "$task - has been added to your list."
+}
+
+delete()
+{
+    if [[ "$id" =~ ^[0-9]+$ ]] && [ "$id" -ne "0" ]; then
+        if [ "$id" -le $(wc -l < $DATA_FILE) ]; then
+            if confirm; then
+                sed -i "${id}d" $DATA_FILE
+                echo "Task deleted successfully"
+            else 
+                echo "Calm down! Nothing is deleted"
+            fi
+        else
+            echo "Entered id does not match"
+        fi
+    else 
+        echo "Invalid input. Enter correct id" 
+    fi
+}
+
+edit()
+{
+    if [[ "$id" =~ ^[0-9]+$ ]] && [ "$id" -ne "0" ]; then
+        if [ "$id" -le $(wc -l < $DATA_FILE) ]; then
+            if confirm; then
+                sed -i "${id}s/.*/${replace_text}/" $DATA_FILE
+                echo "Task updated successfully"
+            else 
+                echo "Nothing's changed, You are still an ugly bitch."
+            fi
+        else
+            echo "Entered id does not match"
+        fi
+    else 
+        echo "Invalid input. Enter correct id" 
+    fi
+}
+
+list()
+{
+ echo "TODO"
 }
 
 usage()
@@ -88,10 +144,18 @@ while [ "$1" != "" ]; do
         -a | --add )            shift
                                 task="$1"
                                 add
+                                exit
                                 ;;
-        -d | --delete )         : #do something
+        -d | --delete )         shift
+                                id="$1"
+                                delete
+                                exit
                                 ;;
-        -e | --edit )           : #do something
+        -e | --edit )           shift
+                                id="$1"
+                                replace_text="$2"
+                                edit
+                                exit
                                 ;;
         -h | --help )           usage
                                 exit
